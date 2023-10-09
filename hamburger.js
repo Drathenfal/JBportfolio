b3240fa74fa6a2671ee4e3134c58aad08727625a
Jake Bignell <jake.bignell@netmatters-scs.com>
2023-10-09 10:41:10 +0100

    Hambugler added
    Hambugler added

---
+++ hamburger.js
@@ -0,0 +1,10 @@
+//Hamburger and Menu
+
+
+const hamMenu = document.querySelector('.hamburger-menu');
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () =>{
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+});
